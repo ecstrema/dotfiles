1ff8bc4be5e6b0119ccbadf8472761513e7a7e74
Rémi Marche <35939574+Marr11317@users.noreply.github.com>
2021-08-11 00:11:28 -0400

    initial commit

---
+++ macos/dock.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
+
+killall Dock
