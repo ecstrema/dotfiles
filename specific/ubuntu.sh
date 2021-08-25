9a8289dcdcdabedcd36503c624fbfff0d7f37121
Rémi Marche <35939574+Marr11317@users.noreply.github.com>
2021-08-25 14:07:13 -0400

    more installations

--- specific/ubuntu.sh
+++ specific/ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Node installation (nvm)
-apt install curl
+apt install curl unzip openjdk-8-jdk tree
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.profile   # reload profile
+source ~/.profile   # load profile
 nvm install lts --reinstall-packages-from=default --latest-npm
 
 # app installs
