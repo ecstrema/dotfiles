4b2ba2f1527fa2416794520b225b1b3d7d8f3da6
Remi Marche <remimarche@gmail.com>
2022-09-14 00:23:10 +0000

    fix: try another fix

--- global/node.sh
+++ global/node.sh
@@ -4,7 +4,7 @@ sudo npm install -g npm@latest
 
 # npm packages (using pnpm)
 npm i -g pnpm
-source /home/codespace/.bashrc
 pnpm setup
+source /home/codespace/.bashrc
 pnpm i -g @antfu/ni
 ni -g typescript nodemon gh-pages license np zx rimraf
