24a2b509083a445a943a95aa50e6820811216cfc
Rémi Marche <35939574+ecstrema@users.noreply.github.com>
2022-09-30 11:41:44 +0200

    remove sudo

--- global/node.sh
+++ global/node.sh
@@ -4,9 +4,9 @@ sudo npm install -g npm@latest
 
 # npm packages (using pnpm)
 npm i -g pnpm
-SHELL=$SHELL pnpm setup
+pnpm setup
 export PNPM_HOME="/home/codespace/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 source /home/codespace/.bashrc
-sudo pnpm i -g @antfu/ni
-sudo ni -g typescript nodemon gh-pages license np zx rimraf
+pnpm i -g @antfu/ni
+ni -g typescript nodemon gh-pages license np zx rimraf
